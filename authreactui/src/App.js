2cedb3d2e78776482300958ceb95198cabf7ef5b
Ibrahim Hossain <outsideworkibrahim@gmail.com>
2022-08-22 21:20:52 +0600

    create authreactui app

---
+++ authreactui/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Layout from './components/pages/layout';
+function App() {
+  return (
+    <>
+    <Layout/>
+    </>
+  );
+}
+
+export default App;
---
+++ authreactui/src/components/pages/layout.js
@@ -0,0 +1,9 @@
+import Navbar from "../Navbar" 
+const Layout = () => {
+    return <> 
+    <Navbar/> 
+    <h1>Home Page</h1>
+    </>
+};
+
+export default Layout;
